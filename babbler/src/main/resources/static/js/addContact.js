6f151f5336baa4a31701b4c0d57431d8943fb784
ferty <polushin1992@gmail.com>
2018-04-16 03:48:01 +0300

    form add new contact -ready.

--- babbler/src/main/resources/static/js/addContact.js
+++ babbler/src/main/resources/static/js/addContact.js
@@ -1,3 +1,28 @@
-$('#modal_add_contact').hidden(function () {
-    $('#modal_window').css('z-index', '1061');
+$(document).ready(function () {
+    $('#add_contact_modal').css('margin-top', ($(window).innerHeight() - 364) / 2);
+});
+
+$(window).resize(function () {
+    $('#add_contact_modal').css('margin-top', ($(window).innerHeight() - 364) / 2);
+});
+
+$('#modal_add_contact').on('hide.bs.modal', function () {
+    $('#modal_window').css('z-index', '1060');
+});
+
+$('.input_field').on('focus', function () {
+    $(this).parent().css('border-bottom', '2px solid #6bace1');
+    $(this).parent().children('.input_label').css('transform', 'scale(.9)');
+});
+
+$('.input_field').on('blur', function () {
+    $(this).parent().css('border-bottom', '1px solid #e6e6e6');
+    if ($(this).val() === '') {
+        $(this).parent().children('.input_label').css('transform', '');
+    }
+});
+
+$('#btn_cancel').click(function () {
+    $('.input_field').val('');
+    $('.input_label').css('transform', '');
 });
